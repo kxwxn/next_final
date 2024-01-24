c40571f5613d6e5fec3a879e447aed1df9eab77c
n0wlk <kxwxn@icloud.com>
2024-01-24 23:39:27 +0900

    api Response.redirect issue needs to be updated

--- src/app/api/brain/archiving/route.ts
+++ src/app/api/brain/archiving/route.ts
@@ -1,6 +1,10 @@
+import { connectDB } from "@/util/db";
+
 export async function POST(request: Request) {
   const formData = await request.formData();
   const title = formData.get("title");
   const content = formData.get("content");
-  return Response.json({ title, content });
+  const db = (await connectDB).db("n0wlk");
+  db.collection("brainPost").insertOne({ title: title, content: content });
+  return Response.redirect("/brain", 302);
 }
